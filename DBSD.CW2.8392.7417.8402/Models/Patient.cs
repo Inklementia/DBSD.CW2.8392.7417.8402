dc6871895cbf23ba474023b9e3eb8ab40cc2fb7e
Caypoh23 <caypoh23@gmail.com>
2021-04-12 12:00:45 +0500

    feat: frontend is developed

--- DBSD.CW2.8392.7417.8402/Models/Patient.cs
+++ DBSD.CW2.8392.7417.8402/Models/Patient.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 
 namespace DBSD.CW2._8392._7417._8402.Models
 {
-    [DataContract]
     public class Patient
     {
         // Some fields are not included such as ids
@@ -18,38 +17,30 @@ namespace DBSD.CW2._8392._7417._8402.Models
 
         public int? Id { get; set; }
 
-        [DataMember]
         [Required]
         [DisplayName("First Name")]
         public string FirstName { get; set; }
 
-        [DataMember]
         [Required]
         [DisplayName("Last Name")]
         public string LastName { get; set; }
 
-        [DataMember]
         [Required]
         [DisplayName("Date of Birth")]
         [DataType(DataType.Date)]
         public DateTime? DoB { get; set; }
 
-        [DataMember]
         public string Occupation { get; set; }
 
-        [DataMember]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public Gender Gender { get; set; }
 
-        [DataMember]
         [Required]
         public string Phone { get; set; }
 
-        [DataMember]
         [Required]
         public string Address { get; set; }
 
-        [DataMember]
         [DisplayName("Registered Date")]
         [DataType(DataType.Date)]
         public DateTime RegisteredDate { get; set; }
@@ -62,32 +53,32 @@ namespace DBSD.CW2._8392._7417._8402.Models
 
         [DisplayName("Ward")]
         public int? WardId { get; set; }
-        [DataMember]
+
         [DisplayName("Emergency Hospitalization")]
         public bool EmergencyHospitalization { get; set; }
-        [DataMember]
+
         [DisplayName("Discharged")]
         public bool IsDischarged { get; set; }
 
         public byte[] Photo { get; set; }
 
-        [DataMember]
+
         [DisplayName("Diagnose name")]
         public string DiagnoseName { get; set; }
 
-        [DataMember]
+
         [DisplayName("Doctor name")]
         public string DoctorName { get; set; }
 
-        [DataMember]
+
         [DisplayName("Department")]
         public string DepartmentName { get; set; }
 
-        [DataMember]
+
         [DisplayName("Ward number")]
         public string WardNo { get; set; }
 
-        [DataMember]
+
         [DisplayName("Nurse name")]
         public string NurseName { get; set; }
     }
